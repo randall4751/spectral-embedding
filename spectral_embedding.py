ec50971dffc9f8361578fec5d7b88f0efd8c7ef0
johnsonrc <johnsonrc@familysearch.org>
2020-05-26 15:49:19 -0600

    updated python code

--- spectral_embedding.py
+++ spectral_embedding.py
@@ -1,66 +1,16 @@
 import numpy as np
 
-#
-#   A represents an adjacency matrix for an undirected, 8-node simple (i.e. no self loops) graph
-#       undirected --> symmetric matrix
-#
-A = np.array([
-    [0,1,0,1,0,0,0,0],
-    [1,0,1,0,1,0,0,0],
-    [0,1,0,1,0,0,0,0],
-    [1,0,1,0,1,0,0,1],
-    [0,1,0,1,0,1,1,0],
-    [0,0,0,0,1,0,1,0],
-    [0,0,0,0,1,1,0,1],
-    [0,0,0,1,0,0,1,0]
-])
-
-def make_grid_adjacency_matrix(n):
-    points = n*n
-    g = np.zeros((points,points), dtype='float')
-
-    for i in range(n):
-        for j in range(n):
-            p = i*n + j
-            if i == 0:
-                if j == 0:
-                    g[p,p+1], g[p,p+n] = (1,1)
-                elif j == n-1:
-                    g[p,p-1], g[p,p+n] = (1,1)
-                else:
-                    g[p,p-1], g[p,p+1], g[p,p+n] = (1,1,1)
-            elif i == n-1:
-                if j == 0:
-                    g[p,p+1], g[p,p-n] = (1,1)
-                elif j == n-1:
-                    g[p,p-1], g[p,p-n] = (1,1)
-                else:
-                    g[p,p-1], g[p,p+1], g[p,p-n] = (1,1,1)
-            else:
-                if j == 0:
-                    g[p,p+1], g[p,p-n], g[p,p+n] = (1,1,1)
-                elif j == n-1:
-                    g[p,p-1], g[p,p-n], g[p,p+n] = (1,1,1)
-                else:
-                    g[p,p-1], g[p,p+1], g[p,p-n], g[p,p+n] = (1,1,1,1)
-    return g
-
 def compute_laplacian(A):
     D = np.diag(np.sum(A, axis=0))
     L = D - A
     return L
 
-def compute_eigen_system(L):
-    values, vectors = np.linalg.eig(L)
-    sorted_value_indexes = sorted(range(len(values)), key=lambda i: values[i])
-    sorted_vectors = vectors[:, sorted_value_indexes]
-    return sorted_vectors
-
 def create_embedding(A, k):
     #
-    #   assert that adjacency matrix is symmetric (i.e. undirected graph)
+    #   assert that adjacency matrix is symmetric (i.e. undirected graph) and
+    #   no self-loops
     #
-    assert(np.all(A == A.T))
+    assert np.all(A == A.T) and np.all(np.diagonal(A) == 0)
     #
     #   compute diagonal degree matrix but summing along an axis
     #
@@ -129,51 +79,3 @@ def partition_graph(embedding):
 def number_of_triangles(A):
     A_3 = np.dot(np.dot(A,A),A)
     return int(np.trace(A_3)/6)
-
-if __name__ == '__main__':
-    import sys
-
-    A = read_edge_list('graphs/simple-8-node-graph-edge-list.txt')
-    assert is_simple_graph(A)
-    A_3 = np.dot(np.dot(A,A),A)
-    print(A_3)
-    sys.exit()
-    
-    D = np.diag(np.sum(A, axis=0))
-    print(D)
-    #
-    #   compute the Laplacian for the graph
-    #       - diagonal is node degree
-    #       - edge = -1
-    #       - no edge = 0
-    L = D - A
-    print(L)
-    sys.exit()
-    
-    A_3 = np.dot(np.dot(A,A), A)
-    print(A_3)
-    print(f'number of triangles: {number_of_triangles(A)}')
-    sys.exit()
-
-    embedding = create_embedding(A,2)
-
-    print(partition_graph(embedding))
-    # print(group_B)
-
-    from sklearn.cluster import KMeans
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(embedding)
-    print()
-    print(kmeans.labels_)
-    
-    print()
-    kmeans = KMeans(n_clusters=3, random_state=0).fit(embedding)
-    print(kmeans.labels_)
-
-    sys.exit()
-
-    import matplotlib.pyplot as plt
-    plt.scatter(embedding[:,1], embedding[:,0])
-    for i in range(A.shape[0]):
-        plt.annotate(str(i), (embedding[i,1], embedding[i,0]))
-    plt.show()
-
